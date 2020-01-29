7f83ebbd9f7a94c149e7fd5218a86b9d61b2bcdc
SAKAI, CHRISTIAN (CORP) <CHRISTIAN.SAKAI@adp.com>
2020-01-29 16:26:00 -0500

    Fix formatting

--- 10_pipeline.go
+++ 10_pipeline.go
@@ -3,35 +3,35 @@ package main
 import "fmt"
 
 func main() {
-  for n := range sq(gen(2, 3)) {
-    fmt.Println(n)
-  }
+	for n := range sq(gen(2, 3)) {
+		fmt.Println(n)
+	}
 }
 
-func gen(nums ...int) <- chan int {
-  out := make(chan int)
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
 
-  go func() {
-    defer close(out)
+	go func() {
+		defer close(out)
 
-    for _, n := range nums {
-      out <- n
-    }
-  }()
+		for _, n := range nums {
+			out <- n
+		}
+	}()
 
-  return out
+	return out
 }
 
-func sq(in <- chan int) <- chan int {
-  out := make(chan int)
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
 
-  go func() {
-    defer close(out)
+	go func() {
+		defer close(out)
 
-    for n := range in {
-      out <- n * n
-    }
-  }()
+		for n := range in {
+			out <- n * n
+		}
+	}()
 
-  return out
+	return out
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-concurrency-pattern
+
+go 1.13
